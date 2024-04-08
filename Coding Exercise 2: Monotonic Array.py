69eece4a9c3ecd81ab6b9c1b091cc62ea1521616
mukeshkrishnar <mkrishnar@tekion.com>
2024-04-08 11:30:25 +0530

    Added 2nd question

---
+++ Coding Exercise 2: Monotonic Array.py
@@ -0,0 +1,20 @@
+def monotonic_array(array):
+    n = len(array)
+    if n == 0:
+        return True
+    first = array[0]
+    last = array[n-1]
+
+    if first < last:
+        # Monotonic Increasing, else Not monotonic
+        for k in range(n-1):
+            if array[k] > array [k+1]: return False
+    elif first == last:
+        # Monotonic if all values are same
+        for k in range(n-1):
+            if array[k] != array[k+1]: return False
+    else:
+        # Monotonic Decreasing, else not monotonic
+        for k in range(n - 1):
+            if array[k] < array[k+1]: return False
+    return True
